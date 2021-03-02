8bf5252e731866ac8d0ba29efe89d2507e5797df
Andrew Pan <known@unown.me>
2021-03-02 01:22:48 -0600

    flake: checkin, initial stab at github actions
    deploys a zola site to the public/ folder on the deploy branch.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-20.09";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem
+    (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ zola ];
+        };
+        packages.site = pkgs.stdenv.mkDerivation {
+          name = "site";
+          src = self;
+
+          buildInputs = with pkgs; [ zola ];
+          dontInstall = true;
+          buildPhase = "zola build -o $out";
+        };
+      });
+}
