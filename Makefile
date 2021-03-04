ca1dc246d63e3209d7f5a5822689bb943caa71e9
Andrew Pan <known@unown.me>
2021-03-03 23:19:13 -0600

    flake: introduce node2nix

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-.DUMMY: dev
+.DUMMY: dev n2n
+
+n2n:
+	node2nix --development -l package-lock.json -c node-attrs.nix
 
 dev:
 	zola serve &
--- flake.nix
+++ flake.nix
@@ -7,19 +7,24 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem
     (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+          nodeDeps = (pkgs.callPackage ./node-attrs.nix {}).shell.nodeDependencies;
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ zola ];
+          buildInputs = with pkgs; [ zola nodejs nodePackages.node2nix ];
         };
         packages.site = pkgs.stdenv.mkDerivation {
           name = "site";
           src = self;
 
-          buildInputs = with pkgs; [ zola ];
+          buildInputs = with pkgs; [ zola nodejs ];
           dontInstall = true;
           buildPhase = ''
-echo "${self.shortRev or "unknown"}" >rev.json
-zola build -o $out'';
+ln -s ${nodeDeps}/lib/node_modules ./node_modules
+export PATH="${nodeDeps}/bin:$PATH"
+npx postcss --env production sass/index.scss -o static/index.css
+echo \"${self.shortRev or "unknown"}\" >rev.json
+zola build -o $out
+'';
         };
       });
 }
