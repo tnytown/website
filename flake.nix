e4c87666feeab325a60637ac29ba364841ec06d8
Andrew Pan <known@unown.me>
2021-07-17 17:11:48 -0500

    flake: fix code block overflow
    Fixes regression introduced in c820dceaecae2382125d78ab9842225c2e4af28d.
    
    The minify_html option wasn't implemented in the Zola version in Nixpkgs 20.09. When I bumped the
    version to 21.05, it took affect and messed with linebreaks in the `<pre>` tag. Overrode Zola to
    achieve the correct results. See getzola/zola#1300.

--- flake.nix
+++ flake.nix
@@ -7,16 +7,35 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem
     (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+          lib = nixpkgs.lib;
           nodeDeps = (pkgs.callPackage ./nix/node-attrs.nix {}).shell.nodeDependencies;
+          zolaNext = pkgs.zola.overrideAttrs(o: rec {
+            # version = "next-2021-07-10";
+            version = "next-2021-02-06";
+            src = pkgs.fetchFromGitHub {
+              owner = "getzola";
+              repo = o.pname;
+              rev = "a65a2d52c70def075d8b4ed4c57dfd81b1f96ba3";
+              /*rev = "8c3ce7d7fbc0d585d4cbf27598ac7dfe5acd96f1";
+              hash = "sha256-Tw3u96ZPb0yUXvtJ+rna6nnb0a+KfTEiR/PPEadFxDA=";*/
+              hash = "sha256-xBzKT6Fdo95b6qFahN+x7v078rFyCsaI0fPZeLLz+iY=";
+            };
+            cargoDeps = o.cargoDeps.overrideAttrs (lib.const {
+              name = "${o.pname}-${version}-vendor.tar.gz";
+              inherit src;
+              /*outputHash = "sha256-eJSu9p/6DJpS5j89OOkpYF3HF6uaJdqOl8zdvUrhGgc=";*/
+              outputHash = "sha256-N79lrFqugJCky43bAKVPumNPqq9DECbmNtdNJR4T0oE=";
+            });
+          });
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ zola nodejs pandoc nodePackages.node2nix ];
+          buildInputs = with pkgs; [ zolaNext nodejs nodePackages.node2nix ];
         };
         packages.site = pkgs.stdenv.mkDerivation {
           name = "site";
           src = pkgs.nix-gitignore.gitignoreSource [] ./.;
 
-          buildInputs = with pkgs; [ zola nodejs ];
+          buildInputs = with pkgs; [ zolaNext nodejs ];
           phases = [ "unpackPhase" "buildPhase" ];
           buildPhase = ''
             ln -s ${nodeDeps}/lib/node_modules ./node_modules
