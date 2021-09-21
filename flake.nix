ff4d10f69f7074fb89c2614f8081501be4022eaf
Andrew Pan <known@unown.me>
2021-09-21 02:01:49 -0500

    cv: checkin

--- flake.nix
+++ flake.nix
@@ -10,41 +10,76 @@
           lib = nixpkgs.lib;
           nodeDeps = (pkgs.callPackage ./nix/node-attrs.nix {}).shell.nodeDependencies;
           zolaNext = pkgs.zola.overrideAttrs(o: rec {
-            # version = "next-2021-07-10";
             version = "next-2021-02-06";
             src = pkgs.fetchFromGitHub {
               owner = "getzola";
               repo = o.pname;
               rev = "a65a2d52c70def075d8b4ed4c57dfd81b1f96ba3";
-              /*rev = "8c3ce7d7fbc0d585d4cbf27598ac7dfe5acd96f1";
-              hash = "sha256-Tw3u96ZPb0yUXvtJ+rna6nnb0a+KfTEiR/PPEadFxDA=";*/
               hash = "sha256-xBzKT6Fdo95b6qFahN+x7v078rFyCsaI0fPZeLLz+iY=";
             };
             cargoDeps = o.cargoDeps.overrideAttrs (lib.const {
               name = "${o.pname}-${version}-vendor.tar.gz";
               inherit src;
-              /*outputHash = "sha256-eJSu9p/6DJpS5j89OOkpYF3HF6uaJdqOl8zdvUrhGgc=";*/
               outputHash = "sha256-N79lrFqugJCky43bAKVPumNPqq9DECbmNtdNJR4T0oE=";
             });
           });
+          texenv = (pkgs.texlive.combine {
+            inherit (pkgs.texlive)
+              scheme-small
+              # color overrides, maybe unnecessary now
+              xcolor
+              # layout
+              arydshln multirow enumitem
+              # lua ...
+              luatex
+              luacode
+              # fonts
+              mathpazo ebgaramond fontawesome5 academicons
+              # documentclass
+              moderncv;
+          });
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ zolaNext nodejs nodePackages.node2nix ];
+          buildInputs = with pkgs; [ texenv tectonic zolaNext nodejs nodePackages.node2nix ];
         };
-        packages.site = pkgs.stdenv.mkDerivation {
-          name = "site";
-          src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+        packages = rec {
+          texdbg = pkgs.mkShell {
+            buildInputs = [ texenv ];
+          };
+          cv = pkgs.stdenvNoCC.mkDerivation {
+            name = "cv";
+            src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+            buildInputs = [ texenv ];
 
-          buildInputs = with pkgs; [ zolaNext nodejs ];
-          phases = [ "unpackPhase" "buildPhase" ];
-          buildPhase = ''
+            phases = [ "buildPhase" ];
+            # YOLO
+            # TODO(tny): can we cache this?
+            TEXMFHOME = "/tmp/.texlive";
+            TEXMFVAR = "/tmp/.texlive/texmf-var";
+
+            buildPhase = ''
+                echo \"${self.shortRev or "HEAD"}\" >rev.json
+                lualatex $src/cv.tex
+                mkdir -p $out && mv cv.pdf $out/
+            '';
+          };
+
+          site = pkgs.stdenvNoCC.mkDerivation {
+            name = "site";
+            src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+
+            buildInputs = with pkgs; [ zolaNext nodejs ];
+            phases = [ "unpackPhase" "buildPhase" ];
+            buildPhase = ''
             ln -s ${nodeDeps}/lib/node_modules ./node_modules
             export PATH="${nodeDeps}/bin:$PATH"
 
             npx postcss --env production sass/index.scss -o static/index.css
-            echo \"${self.shortRev or "unknown"}\" >rev.json
+            cp ${cv}/cv.pdf static/
+            echo \"${self.shortRev or "HEAD"}\" >rev.json
             zola build -o $out
           '';
+          };
         };
       });
 }
